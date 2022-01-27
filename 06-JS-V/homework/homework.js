4b5da17b3d84dfd0992ce5b49dc3c52c0b6fdc13
Octavio Carchano <you@example.com>
2022-01-27 16:24:10 -0300

    JSV Resueltos

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -78,7 +78,7 @@ function crearInstanciaPersona(nombre, apellido, edad, dir) {
   //Con esta función vamos a crear una nueva persona a partir de nuestro constructor de persona (creado en el ejercicio anterior)
   //Recibirá los valores "Juan", "Perez", 22, "Saavedra 123" para sus respectivas propiedades
   //Devolver la nueva persona creada
-  let juan = new Persona("Juan", "Perez", "22", "Saavedra 123");
+  let juan = new Persona("Juan", "Perez", 22, "Saavedra 123");
   return juan;
 }
   
@@ -87,7 +87,7 @@ function agregarMetodo() {
   // y la edad de la persona y devuelve: 
   //Ej: "Juan, 22 años"
   Persona.prototype.datos = function(){
-    return this.nombre + ", " + this.edad + " años";
+    return this.nombre + ", " + this.edad + " años";  
   };
 }
   
